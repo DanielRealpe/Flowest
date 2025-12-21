31f892bc49fdbc2be5968ef24dd4801b7d81417d
DanielRealpe <daniel_realpee@soy.sena.edu.co>
2025-12-21 14:42:12 -0500

    Fixing to aprove lint

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,17 +1,17 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
-const SECRET = process.env.JWT_SECRET || 'tu_secreto_por_defecto';
+const SECRET = process.env.JWT_SECRET || "tu_secreto_por_defecto";
 
-export function authenticateToken(req, res, next) {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1]; // Espera formato "Bearer <token>"
+export function authenticateToken (req, res, next) {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1]; // Espera formato "Bearer <token>"
 
   if (!token) {
-    return res.status(401).json({ message: 'Token requerido' });
+    return res.status(401).json({ message: "Token requerido" });
   }
 
   jwt.verify(token, SECRET, (err, user) => {
-    if (err) return res.status(403).json({ message: 'Token inválido' });
+    if (err) return res.status(403).json({ message: "Token inválido" });
     req.user = user;
     next();
   });
--- src/modules/categories/router/category.js
+++ src/modules/categories/router/category.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { authenticateToken } from '../../../middleware/auth.js';
+import { authenticateToken } from "../../../middleware/auth.js";
 import { categoryController } from "../controller/category.js";
 
 const router = Router();
